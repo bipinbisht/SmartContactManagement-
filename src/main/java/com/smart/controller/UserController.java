25af9083c19fb8dd6e13e3f3e992da8cd54bd819
Bipin Bisht <bipin@originitsolutions.com>
2021-02-25 18:24:28 +0530

    mypush

--- src/main/java/com/smart/controller/UserController.java
+++ src/main/java/com/smart/controller/UserController.java
@@ -1,14 +1,30 @@
 package com.smart.controller;
 
+import java.security.Principal;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.smart.dao.UserRepository;
+import com.smart.models.User;
 
 @Controller
 @RequestMapping("user")
 public class UserController {
+	@Autowired
+	private UserRepository userRepo;
 	@RequestMapping("index")
-	public String dashBoard()
+	public String dashBoard(Model m,Principal p)
 	{
+		String userName = p.getName();
+		System.out.println("USERNAME=:"+userName);
+		//get the user using username(email)
+		User user = userRepo.getUserByName(userName);
+		System.out.println("User "+user);
+		
+		m.addAttribute("user", user);
 		return "normal/user_dashboard";
 	}
 }
